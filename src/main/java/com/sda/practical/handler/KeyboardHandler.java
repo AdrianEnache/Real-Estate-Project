2bed9a32a16b61dba4b1c6ca4c87064bf85b65b9
Cuza Sergiu <cuza.sergiu@yahoo.com>
2020-10-24 15:02:24 +0300

    Keyboardhandler - done

--- src/main/java/com/sda/practical/handler/KeyboardHandler.java
+++ src/main/java/com/sda/practical/handler/KeyboardHandler.java
@@ -1,4 +1,34 @@
 package com.sda.practical.handler;
 
+import java.util.Scanner;
+
 public class KeyboardHandler {
+
+    private Scanner scanner;
+
+    public KeyboardHandler() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public String readString(String label) {
+        System.out.println(label);
+        return scanner.nextLine();
+    }
+
+    public Integer readInteger(String label) {
+        Integer value = -1;
+        boolean stare = true;
+        do {
+            System.out.println(label);
+            if (!scanner.hasNextInt()) {
+                System.out.println("That is not a number!");
+                scanner.next();
+            } else {
+                value = scanner.nextInt();
+                scanner.nextLine();
+                stare = false;
+            }
+        } while (stare);
+        return value;
+    }
 }
