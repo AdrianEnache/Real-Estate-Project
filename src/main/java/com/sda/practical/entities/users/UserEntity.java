d815bbea12ad5a8842336eb7d44bcb5efe654be8
DESKTOP-2AJEG8T\Toshiba <buzatubianca.denisia@yahoo.com>
2020-10-24 13:35:17 +0300

    UserEntity creat

---
+++ src/main/java/com/sda/practical/entities/users/UserEntity.java
@@ -0,0 +1,49 @@
+package com.sda.practical.entities.users;
+
+public class UserEntity {
+    private Integer userId;
+    private String surname;
+    private String name;
+    private String phoneNumber;
+    private String email;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
