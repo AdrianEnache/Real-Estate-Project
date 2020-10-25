172f4f48398b114f7b2bd60b44071f069db6daac
Adrian Enache <constantin.adrian.enache@gmail.com>
2020-10-25 15:15:51 +0200

    AddImobil-update

--- src/main/java/com/sda/practical/handler/DatabaseHandler.java
+++ src/main/java/com/sda/practical/handler/DatabaseHandler.java
@@ -1,8 +1,9 @@
 package com.sda.practical.handler;
 
 import com.sda.practical.entities.imobile.*;
-import com.sda.practical.entities.users.UserEntity;
+
 import com.sda.practical.entities.users.UserTypesEntity;
+import com.sda.practical.entities.users.UtilizatorEntitate;
 import com.sda.practical.models.FilterModel;
 import com.sda.practical.models.ImobilModel;
 import com.sda.practical.models.UserModel;
@@ -13,6 +14,7 @@ import org.hibernate.Transaction;
 import java.util.Arrays;
 import java.util.List;
 
+
 public class DatabaseHandler {
 
 
@@ -20,16 +22,16 @@ public class DatabaseHandler {
         Transaction transaction = null;
         try {
             Session session = HibernateUtil.getSessionFactory().openSession();
-            UserEntity userEntity = new UserEntity();
-            userEntity.setName(userModel.getName());
-            userEntity.setSurname(userModel.getSurname());
-            userEntity.setPassword(userModel.getPassword());
-            userEntity.setEmail(userModel.getEmail());
-            userEntity.setPhoneNumber(userModel.getPhoneNumber());
+            UtilizatorEntitate utilizator = new UtilizatorEntitate();
+            utilizator.setPrenume(userModel.getName());
+            utilizator.setNumeDeFamilie(userModel.getSurname());
+            utilizator.setParola(userModel.getPassword());
+            utilizator.setAdresaEmail(userModel.getEmail());
+            utilizator.setNumarTelefon(userModel.getPhoneNumber());
             UserTypesEntity userTypesEntity = session.find(UserTypesEntity.class,userModel.getUserTypeId());
-            userEntity.setUserType(userTypesEntity);
+            utilizator.setUserType(userTypesEntity);
             transaction = session.beginTransaction();
-            session.save(userEntity);
+            session.save(utilizator);
             transaction.commit();
         } catch (Exception ex) {
             if (transaction != null) {
@@ -44,25 +46,25 @@ public class DatabaseHandler {
         Transaction transaction = null;
         try{
             Session session = HibernateUtil.getSessionFactory().openSession();
-            ImobileEntity imobileEntity = new ImobileEntity();
-            imobileEntity.setDataPostariiAnuntului(imobilModel.getDataPostariiAnuntului());
-            imobileEntity.setPret(imobilModel.getPret());
-            imobileEntity.setSuprafata(imobilModel.getSuprafata());
-            imobileEntity.setEtaj(imobilModel.getEtaj());
-            imobileEntity.setAnConstructii(imobilModel.getAnConstructie());
-            imobileEntity.setNumarCamere(imobilModel.getNumarCamere());
-            imobileEntity.setCoordonate(imobilModel.getCoordonate());
-            imobileEntity.setDescriere(imobilModel.getDescriere());
-            AnuntStatusEntity anuntStatus = session.find(AnuntStatusEntity.class,imobilModel.getIdAnuntStatusEntity());
-            imobileEntity.setAnuntStatusEntity(anuntStatus);
-            CompartimentareEntity compartimentare = session.find(CompartimentareEntity.class, imobilModel.getIdCompartimentareEntity());
-            imobileEntity.setCompartimentareEntity(compartimentare);
-            CurrencyEntity currency = session.find(CurrencyEntity.class,imobilModel.getIdCurrencyEntity());
-            imobileEntity.setCurrencyEntities(Arrays.asList(currency));
-            OrasEntity oras = session.find(OrasEntity.class,imobilModel.getIdOras());
-            imobileEntity.setOrasEntity(oras);
+            ImobileEntitate imobil = new ImobileEntitate();
+            imobil.setDataPostariiAnuntului(imobilModel.getDataPostariiAnuntului());
+            imobil.setPret(imobilModel.getPret());
+            imobil.setSuprafata(imobilModel.getSuprafata());
+            imobil.setEtaj(imobilModel.getEtaj());
+            imobil.setAnConstructii(imobilModel.getAnConstructie());
+            imobil.setNumarCamere(imobilModel.getNumarCamere());
+            imobil.setCoordonate(imobilModel.getCoordonate());
+            imobil.setDescriere(imobilModel.getDescriere());
+            AnuntStatusEntitate anuntStatus = session.find(AnuntStatusEntitate.class,imobilModel.getIdAnuntStatusEntity());
+            imobil.setAnuntStatusEntity(anuntStatus);
+            CompartimentareEntitate compartimentare = session.find(CompartimentareEntitate.class, imobilModel.getIdCompartimentareEntity());
+            imobil.setCompartimentareEntity(compartimentare);
+            ValutaEntitate currency = session.find(ValutaEntitate.class,imobilModel.getIdCurrencyEntity());
+            imobil.setCurrencyEntities(Arrays.asList(currency));
+            OrasEntitate oras = session.find(OrasEntitate.class,imobilModel.getIdOras());
+            imobil.setOrasEntity(oras);
             transaction = session.beginTransaction();
-            session.save(imobileEntity);
+            session.save(imobil);
             transaction.commit();
             session.close();
 
@@ -78,7 +80,7 @@ public class DatabaseHandler {
 
 
 
-    public List<ImobileEntity> getImobils(FilterModel filterModel) {
+    public List<ImobileEntitate> getImobils(FilterModel filterModel) {
         String sql = createFilterSql(filterModel);
 
         return null;
