a8784e56d6f7271235ac65736bfbf1eda824fd1a
Cuza Sergiu <cuza.sergiu@yahoo.com>
2020-10-25 12:07:28 +0200

    Merge branch 'master' of https://gitlab.com/javaremote7-grupa1/practical-project-grupa-1/imobiliare into master
    # Conflicts:
    #	src/main/java/com/sda/practical/handler/DatabaseHandler.java
    #	src/test/java/com/sda/practical/HibernateUtilTest.java

---
+++ src/main/java/com/sda/practical/models/FiltrerModel.java
@@ -0,0 +1,23 @@
+package com.sda.practical.models;
+
+public class FiltrerModel {
+
+    private Integer suprfata;
+    private String oras;
+
+    public Integer getSuprfata() {
+        return suprfata;
+    }
+
+    public void setSuprfata(Integer suprfata) {
+        this.suprfata = suprfata;
+    }
+
+    public String getOras() {
+        return oras;
+    }
+
+    public void setOras(String oras) {
+        this.oras = oras;
+    }
+}
