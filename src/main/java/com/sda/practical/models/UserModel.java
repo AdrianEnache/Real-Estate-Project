b4d3c6794ed6a42a1355afd642f544284920c8c4
Adrian Enache <constantin.adrian.enache@gmail.com>
2020-10-25 12:57:49 +0200

    ImobilModel creat UserModel creat creatuser() - update addImobil() - creat

---
+++ src/main/java/com/sda/practical/models/UserModel.java
@@ -0,0 +1,61 @@
+package com.sda.practical.models;
+
+
+
+public class UserModel {
+
+    private String surname;
+    private String name;
+    private String phoneNumber;
+    private String email;
+    private String password;
+    private Integer userTypeId;
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getUserTypeId() {
+        return userTypeId;
+    }
+
+    public void setUserTypeId(Integer userTypeId) {
+        this.userTypeId = userTypeId;
+    }
+}
